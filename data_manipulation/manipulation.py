fffbb50b17ff56e84191725a91b441d0d0bed9a9
omid-sar <mr.omid.sardari@gmail.com>
2024-04-26 17:03:15 -0400

    update

---
+++ data_manipulation/manipulation.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+dfo = pd.read_csv('../data/P87-S7-Advanced-Manipulation-Resources/flights2.csv.gz')
+print(dfo.columns)
+dfo.head()
+
