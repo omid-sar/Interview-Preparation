97c6c2666a7ae7b401c490e9794b91c1b6c3b5e7
omid-sar <mr.omid.sardari@gmail.com>
2024-04-26 10:08:01 -0400

    knn

--- hands-on_machine_learning/Python/knn.py
+++ hands-on_machine_learning/Python/knn.py
@@ -9,16 +9,13 @@ iris = load_iris()
 df_iris = pd.DataFrame(iris.data, columns=iris.feature_names)
 df_iris["target"] = iris.target
 
-df_iris
-
 sns.pairplot(df_iris.reset_index(drop=True), hue="target")
 
 
-X = df_iris.drop("target", axis=1)
+X = df_iris.drop("target", axis=1, inplace=False )
 y = df_iris["target"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05, random_state=42)
-X_new = X_test.reset_index(drop=True).iloc[0]
-
+X_new = X_test.reset_index(drop=True).iloc[1]
 
 
 # Find knn (3-nearest neighbors) based on euclidean distance
@@ -31,28 +28,72 @@ def euclidian_nearest_neighbors (X: float, X_new: float, k:int) -> float:
 
 
 # Predict output by averaging k nearest neighbors
-def knn_predict()
+def knn_predict(top_k: pandas.series , Y: float) -> float:
 
-np.sqrt(15)
+    pass
 
-X_train - X_new
 
-(X_train - X_new)*((X_train - X_new).T)
 
-(np.square(X_train - X_new))
-
-np.sum(np.square(X_train - X_new), axis=1, sort=ascending)
-np.sort(np.sum(np.square(X_train - X_new), axis=1))
 
 squared_diff= np.square(X_train - X_new)
 # we dont need to calculare square root since the order is going to be the same.
 distance = np.sum(squared_diff, axis=1)
 distance_sorted = np.argsort(distance)
-distance.sort_values()[:3]
+top_k = distance.sort_values()[:3]
 
+
+y_train
+top_k_index = top_k.index
+y_nearset_k = y_train.loc[top_k_index]
+
+y_nearset_k.value_counts()
+
+
+randomm = pd.Series(np.random.randint(0, 10, 100))
+randomm.value_counts()
+randomm.value_counts().argmax()
+
+
+type(top_k)
 X_train["sepal length (cm)"][68]
 X_train.iloc[68]
 X_train.loc[68]
 y_test.
 
+x = np.linspace(0,10,100)
+y = np.sin(x)
 
+plt.figure(figsize=(6,10))
+
+
+plt.subplot(3,1, 1)
+plt.scatter(x,y)
+
+
+plt.subplot(3,1, 2)
+plt.plot(x,y)
+
+
+plt.subplot(3,1, 3)
+plt.hist(y, bins=30)
+plt.tight_layout()
+plt.show()
+plt.savefig("plot.png")
+
+df =df_iris.groupby(["target"]).mean().reset_index()
+
+df.columns
+
+Index(['target', 'sepal length (cm)', 'sepal width (cm)', 'petal length (cm)',
+       'petal width (cm)'],
+      dtype='object')
+
+plt.bar(df_iris['target'].values, df_iris['target'].index)
+
+plt.bar( df_iris['target'].index, df_iris['target'].values)
+df_iris["target"]
+
+
+plt.plot(df_iris.index ,df_iris["sepal length (cm)"], df_iris["sepal width (cm)"])
+plt.scatter(df_iris["sepal width (cm)"], df_iris["sepal length (cm)"])
+plt.bar( df['target'], df['sepal width (cm)'], label='age', color=["red", "blue"])
