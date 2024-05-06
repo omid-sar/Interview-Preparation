9cacc06053fe9b52cdd9d1d6707c589939a388e1
omid-sar <mr.omid.sardari@gmail.com>
2024-05-06 16:40:10 -0400

    LLE (Localy Linear Embedding)

--- hands-on_machine_learning/Python/PCA.py
+++ hands-on_machine_learning/Python/PCA.py
@@ -1,5 +1,31 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA 
 
 
 pca = PCA(n_components=2)
+
+
+# --------------------------- LLE (Localy Linear Embedding) ------------------------ #
+from sklearn.manifold import LocallyLinearEmbedding
+from sklearn.datasets import make_swiss_roll
+from mpl_toolkits.mplot3d import Axes3D
+
+X_swiss, t = make_swiss_roll(n_samples=10000, noise=0.2, random_state=42)
+
+fig = plt.figure(figsize=(10,6))
+ax = fig.add_subplot(111, projection="3d")
+scatter = ax.scatter(X_swiss[:,0], X_swiss[:,1], X_swiss[:,2], c=t, cmap='Spectral')
+ax.set_title(" 3D Swiss Roll Dataset")
+plt.show()
+
+
+lle = LocallyLinearEmbedding(n_components=2, n_neighbors=10, random_state=42)
+X_unrolled = lle.fit_transform(X_swiss)
+
+fig, ax  = plt.subplots(figsize=(10,6))
+scatter= ax.scatter(X_unrolled[:,0], X_unrolled[:,1], c=t, cmap='Spectral')
+ax.set_title(" UNROLLED 3D Swiss Roll Dataset")
+plt.show()
+
+
